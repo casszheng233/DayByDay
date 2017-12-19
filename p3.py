db7e1fa4374b454565bcb98ec224b1de965eeaac
Cassandra Zheng <Cassandra Zheng>
2017-12-18 22:13:55 -0500

    p3.py commented

--- p3.py
+++ p3.py
@@ -1,3 +1,8 @@
+# CS304 Final Project
+# Beta Version
+# p3.py
+# Rosie Pyktel and Cassandra Zheng
+
 import dbconn2
 import MySQLdb
 import os
@@ -5,18 +10,23 @@ from flask import (Flask, render_template, url_for, request, flash)
 from DSN import *
 # import id
 
+#a helper function to get connection
 def getConnection():
     dsn = rpyktel_dsn
     conn = dbconn2.connect(dsn)
     return conn
 
+#a helper function to check if the username and password match with the database record
+#used in login
 def checkUser(conn,username,passwd):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    print 'connected'
+    # print 'connected'
     curs.execute('select * from user where username = %s and psswd = %s;',(username,passwd,))
     row = curs.fetchone()
     return row
 
+#a helper function to check if a username has already been used in DATABASE
+#used in registering new users
 def checkUsername(conn,username):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     print 'connected'
@@ -26,11 +36,13 @@ def checkUsername(conn,username):
         return True
     else:
         return False
-
+#a helper function to create a new user
+#used in user registration
 def createUser(conn,username,passwd):
     curs  = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('insert into user (username,psswd) values (%s,%s);',(username,passwd,))
 
+#a helper function to get the corresponding color of the indicated category stoerd by the user
 def getCatColors(conn,userID):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('select name, color from category where userID = %s;',(userID,))
@@ -45,15 +57,12 @@ def getCatColors(conn,userID):
 def rightPanelTask(conn,userID):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     getColors = getCatColors(conn,userID)
-    # print 'getCOlors'
-    # print getColors
     curs.execute('select distinct parentTaskID, subTaskID from taskList where userId = %s;',(userID,))
     buildAssociation = curs.fetchall()
     finalData = []
     taskDic = {}
     for object in buildAssociation:
         parentTask = str(object['parentTaskID'])
-        # print parentTask
         subTask = str(object['subTaskID'])
         if (parentTask in taskDic):
             taskDic[parentTask].append(subTask)
@@ -61,7 +70,7 @@ def rightPanelTask(conn,userID):
             taskDic[parentTask] = [subTask]
     for obj in taskDic.keys():
         taskHolder = []
-        #curs1 = cursor(DATABASE)
+        #check for parent task
         curs.execute('select isFinished, taskName, taskID, start, end, name from task where taskID = %s',(obj,))
         parent = curs.fetchall()
 
@@ -74,8 +83,8 @@ def rightPanelTask(conn,userID):
                     'cat': str(parent[0]['name'])}
 
         subDics = []
+        #check for subtasks
         for subID in taskDic[obj]:
-            #curs2 = cursor(DATABASE)
             curs.execute('select taskName, isFinished, taskID, end, name from task where taskID = %s;',(subID,))
             subTask = curs.fetchall()
 
@@ -93,27 +102,28 @@ def rightPanelTask(conn,userID):
             subDics.append(subDic)
         taskHolder = [parentDic,subDics]
         finalData.append(taskHolder)
+    #convert to a javascript-friendly format
     clean1 = str(finalData).replace('datetime.date(','[')
     clean2 = str(clean1).replace(')',']')
     clean3 = str(clean2).replace('None','0')
     return clean3
 
-
+#a helper to build dropdown in base.html
 def buildDropdown(timeSelection,dataSelection):
     data = [['value="day"','Day View'],['value="week"','Week View'],['value="month"','Month View']]
     return data
 
-
-def getCats(conn,userID):#check this part, need to check for none value
+#check for all categories stored by a user
+def getCats(conn,userID):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('select * from category where userID = %s;',(userID,))
     allCats = curs.fetchall()
     if allCats!=None:
-
         return allCats
     else:
         return []
 
+#a helper to insert category into database
 def addCat(conn,name,color,userID):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     try:
@@ -122,10 +132,9 @@ def addCat(conn,name,color,userID):
     except:
         return "error"
 
-
+#insert tasks
 def addTask(conn,isFinished,userID,taskName,start,end,cat):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    print "at addTask"
     try:
         curs.execute('select * from task where taskName = %s and start = %s and end = %s and userID = %s;',(taskName,start,end,userID,))
         row = curs.fetchone()
@@ -140,7 +149,7 @@ def addTask(conn,isFinished,userID,taskName,start,end,cat):
         flash("check your input!")
         print "empty inputs"
 
-
+#recursively delete tasks, not used but should work
 def deleteSubtask(conn,taskID):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('select * from taskList where parentTaskID = %s;',(taskID,))
@@ -155,7 +164,7 @@ def deleteSubtask(conn,taskID):
         curs.execute('delete from taskList where subTaskID = %s;',(taskID,))
         curs.execute('delete from task where taskID  = %s;',(taskID,))
 
-
+#delete tasks
 def deleteTask(conn,userID,taskName,start,end,cat):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     taskID = checkTaskID(taskName,start,end)
@@ -165,10 +174,10 @@ def deleteTask(conn,userID,taskName,start,end,cat):
     else:
         flash('Such task does not exist')
 
+#a helper function to report the system assigned taskID given necessary information
 def checkTaskID(conn,taskName,start,end):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     try:
-        #curs.execute('insert into task(isFinished,userID,taskName,start,end) values ("{0}","{1}","{2}","{3}","{4}");'.format(isFinished,userID,taskName,start,end))
         curs.execute('select * from task where taskName = %s and start = %s and end = %s;',(taskName,start,end,))
         row = curs.fetchone()
         return row
@@ -176,6 +185,7 @@ def checkTaskID(conn,taskName,start,end):
         flash('please check your input')
         print "to do: not working"
 
+#update isFinished status in task
 def tickBox(conn,taskID):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('select isFinished from task where taskID =' + taskID[4:])
@@ -188,60 +198,65 @@ def tickBox(conn,taskID):
     print 'update task set isFinished=' + str(isFin) + ' where taskID = ' + taskID[4:] + ';'
     curs.execute('update task set isFinished=' + str(isFin) + ' where taskID = ' + taskID[4:] + ';')
 
-
+#a helper to add subtask
 def addSubtask(conn,userID,parent,child):#this needs to be an id
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('insert into taskList (userID,parentTaskID,subTaskID) values (%s,%s,%s);',(userID,parent,child,))
 
+#handles special case when a task doesn't have any subtasks
 def addSubtaskNull(conn,userID,parent):#this needs to be an id
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('insert into taskList (userID,parentTaskID,subTaskID) values (%s,%s,NULL);',(userID,parent,))
 
+#a helper to check all categories/colors from a user's database
 def checkCatColor(conn,userID):#check all colors
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('select * from category where userID = %s;',(userID,))
     rows = curs.fetchall()
     return rows
 
+#helper: adding log
 def addLog(conn,cat,hours,userID,taskDate):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute('select * from category where name = %s;',(cat,))
+    curs.execute('select * from category where name = %s;',(cat,))#check if category exists
     row = curs.fetchone()
     if row != None:
         curs.execute('select * from logEntry where name = %s and taskDate = %s and userID = %s;',(cat,taskDate,userID,))
         row = curs.fetchone()
-        if row == None:
+        if row == None:#new log, needs to add in database
             curs.execute('insert into logEntry values (%s,%s,%s,%s);',(cat,hours,userID,taskDate,))
-            # flash('log has been entered')
-        else:
-            #existingHour = row['hours']
+        else:#existing log, needs to update in table
             curs.execute('update logEntry set hours = hours + %s where name = %s and taskDate = %s and userID = %s;',(hours,cat,taskDate,userID,))
             flash('log has been updated ')
-    else:
+    else:#cannot find category
         flash('please input an existing category')
 
-
+#a helper to query database on information based one specific category
 def checkLog(conn,logType,userID,cat):#given logType and category, returns [[yyyy,mm,dd,hours]]
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    #finalResult = []
     if cat == 'all':
         if logType == 'day':
+            #sum of hours of all categories per day
             curs.execute('select taskDate,sum(hours) as hours from logEntry where userID = %s group by taskDate;',(userID,))
-        #if logType == 'day':#per week
-        #    curs.execute('select taskDate, sum(hours) as accum from logEntry where taskDate between date_sub(now(),INTERVAL 1 WEEK) and now() group by taskDate;')
         if logType == 'week':
+            #sum of hours of all categories per week
             curs.execute('select taskDate, sum(hours) as hours from logEntry where userID = %s group by YEARWEEK(taskDate);',(userID,))
         if logType == 'month':
+            #sum of hours of all categories per month
             curs.execute('select taskDate, sum(hours) as hours from logEntry where userID = %s group by YEAR(taskDate), MONTH(taskDate);',(userID,))
     else:
         if logType == 'day':
+            #entered hours of a specifc category per day
             curs.execute('select taskDate,hours from logEntry where userID = %s and name = %s group by taskDate;',(userID,cat,))
         if logType == 'week':
+            #entered hours of a specifc category per week
             curs.execute('select taskDate, hours from logEntry where userID = %s and name = %s group by YEARWEEK(taskDate);',(userID,cat,))
         if logType == 'month':
+            #entered hours of a specifc category per month
             curs.execute('select taskDate, hours from logEntry where userID = %s and name = %s group by YEAR(taskDate), MONTH(taskDate);',(userID,cat,))
     rows = curs.fetchall()
     result = []
+    #result: [[yyyy,mm,dd,hours]]
     if rows!=None:
         for eachrow in rows:
             recDate = eachrow['taskDate']
@@ -251,66 +266,56 @@ def checkLog(conn,logType,userID,cat):#given logType and category, returns [[yyy
                 result.append(cleanRec)
     return result
 
+#a helper (for ajax) returns a list of four dictionaries of:
+#logDictDay: log record of all categories on a daily basis {cat:hours}
+#logDictWeek: log record of all catergories on a weekily basis {cat:hours}
+#logDictMonth: log record of all categories on a monthly basis {cat:hours}
+#colorDict:a record of all categories and their corresponding colors {cat:color}
 def allLog(conn,userID,allCats):
-
-    print "at allLog"
-
     logDictDay = {}
-    logDictDay['all'] = checkLog(conn,'day',userID,'all')
+    logDictDay['all'] = checkLog(conn,'day',userID,'all')#sum of log hours, same as below
     for eachCat in allCats:
         cat = eachCat['name']
-
-        catInfo = checkLog(conn,'day',userID,cat)#list of dictionary
+        catInfo = checkLog(conn,'day',userID,cat)#log hours of a specific cat, same as below
         logDictDay[str(cat)] = checkLog(conn,'day',userID,cat)
 
     logDictWeek = {}
     logDictWeek['all'] = checkLog(conn,'week',userID,'all')
     for eachCat in allCats:
         cat = eachCat['name']
-
-        catInfo = checkLog(conn,'week',userID,cat)#list of dictionary
+        catInfo = checkLog(conn,'week',userID,cat)
         logDictWeek[str(cat)] = checkLog(conn,'week',userID,cat)
 
     logDictMonth = {}
     logDictMonth['all'] = checkLog(conn,'month',userID,'all')
     for eachCat in allCats:
         cat = eachCat['name']
-
-        catInfo = checkLog(conn,'month',userID,cat)#list of dictionary
+        catInfo = checkLog(conn,'month',userID,cat)
         logDictMonth[str(cat)] = checkLog(conn,'month',userID,cat)
 
     colorRows = checkCatColor(conn,userID)
     colorDict = {}
-    colorDict['all']='000000'
+    colorDict['all']='000000'#all (sum of every categories): black
     if colorRows != None:
         for eachRow in colorRows:
             colorDict[str(eachRow['name'])]=str(eachRow['color'])
     return [logDictDay,logDictWeek,logDictMonth,colorDict]
 
+#adding new event for a user given eventName, eventDate, start and end
 def addEvent(conn,userID,eventName,eventDate,start,end):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    #checking if there exists any potential conflict
     curs.execute('select * from event where eventDate = %s and (%s between start and end or %s between start and end);',(eventDate,start,end,))
     row = curs.fetchone()
-    if row == None:
+    if row == None:#if no conflict, then insert the new event
         curs.execute('insert into event (userID,eventDate,start,end,name) values (%s,%s,%s,%s,%s);',(userID,eventDate,start,end,eventName,))
         print 'successfullt insert'
         flash ("Inserted "+eventName +" successfully!")
-    else:
+    else:#flash error message if conflict happens
         print "conflict!"
         flash('there seems to be a scheduling conflict')
         return "error"
-    # try:
-    #     #check if there exists any preexisting event
-    #     curs.execute('select * from event where eventDate = %s where %s bewteen start and end or %s bewteen start and end;',(eventDate,start,end,))
-    #     row = curs.fetchone()
-    #     if row == None:
-    #         curs.execute('insert into event (userID,eventDate,start,end,name) values (%s,%s,%s,%s);',(userID,eventDate,start,end,eventName,))
-    #         print 'successfullt insert'
-    #         flash ("Inserted "+eventName +" successfully!")
-    #     else:
-    #         flash ("task existed in the database")
-    # except:
-    #     flash("check your input!")
+
 
 
 def rightPanelEvent(conn,   userID):
